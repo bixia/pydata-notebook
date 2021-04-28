da93347201160f62cb3308fa17fa94a1f9c058e6
qiwen <343224563@qq.com>
2021-04-28 11:43:24 +0800

    init

---
+++ -- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+SELECT id, pair_name, block_timestamp, actual_price_a2b, actual_price_b2a, amount0In, amount0Out, amount1In, amount1Out, transaction_hash, block_number, log_index, contract_address, sender_addr, to_addr
+FROM uniswap_swap;
